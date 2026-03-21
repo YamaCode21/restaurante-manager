c88952e5f7cef25b4b1efabe932fe714625db0f5
YamaCode21 <bradt.chancan2@gmail.com>
2026-03-21 06:53:18 -0500

    Subiendo proyecto

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
+
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000');
+});
